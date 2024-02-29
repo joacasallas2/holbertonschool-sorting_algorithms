6db50ad7a02d8cfebd23d2e2ddc642548ab7b44d
joacasallas2 <proyecta.ds@gmail.com>
2024-02-29 12:08:25 -0500

    big O

--- sort.h
+++ sort.h
@@ -11,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -30,8 +30,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
-
-
 
 #endif
